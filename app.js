16208b4846bcf52d6fa7c3083d6e85fac962a263
nguyen phu hai <nguyenphuhai89@gmail.com>
2022-11-29 23:26:50 +0700

    commit lab1.5

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const fs = require('fs');
 const http = require('http');
 
 const server =http.createServer((req, res)=>{
     console.log(req.url,req.method, req.headers);
     const url =req.url;
+    const method = req.method;
     if(url==='/'){
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
         res.write('</html>');
+        return res.end();
+    }
+    if(url ==='/message' && method === 'POST'){
+        fs.writeFileSync('message.txt','DUMMY')
+        res.statusCode =302
+        res.setHeader('Location','/')
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
