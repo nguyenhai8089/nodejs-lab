30f6d43e6b18c6c10bb911a3f1bf68decbf8cf5c
nguyen phu hai <nguyenphuhai89@gmail.com>
2022-12-02 11:25:07 +0700

    commit lab1.7

---
+++ routes.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const requestHandler = (req, res)=>{
+    /*  console.log(req.url,req.method, req.headers); */
+     const url =req.url;
+     const method = req.method;
+     if(url==='/'){
+         res.write('<html>');
+         res.write('<head><title>Enter Message</title></head>')
+         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
+         res.write('</html>');
+         return res.end();
+     }
+     if(url ==='/message' && method === 'POST'){
+         const body = []
+         req.on('data',(chunk)=>{
+             console.log(chunk)
+             body.push(chunk)
+         })
+         req.on('end',()=>{
+             const parserBody = Buffer.concat(body).toString();
+             console.log(parserBody)
+             const message = parserBody.split('=')[1];
+             fs.writeFileSync('message.txt',message)
+         })
+         
+         res.statusCode =302
+         res.setHeader('Location','/')
+         return res.end();
+     }
+     res.setHeader('Content-Type', 'text/html');
+     res.write('<html>');
+     res.write('<head><title>My First Page</title></head>')
+     res.write('<body><h1>hello from my Node.js Server!</h1></body>')
+     res.write('</html>');
+     res.end();
+};
+/* module.exports =requestHandler; */
+/*  module.exports = {
+    handler: requestHandler,
+    someText:'Some hard coded text'
+ } */
+ module.exports.handler = requestHandler;
+
+ module.exports.someText = 'Some hard coded text';
