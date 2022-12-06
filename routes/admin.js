e665bca27e2cab300cece9afadac6f6a9c1fbf3c
nguyen phu hai <nguyenphuhai89@gmail.com>
2022-12-07 04:18:35 +0700

    commit lab2.9

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,11 @@
 const express  = require('express');
 const router = express.Router();
-
+// admin/add-product => GET
 router.get('/add-product',(req,res,next)=>{
     console.log('1...in the middleware1!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>')
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>')
 })
-
+// admin/product =>POST
 router.post('/product',(req,res,next)=>{
     console.log(req.body,'2...in another middleware!');
     res.redirect('/')
