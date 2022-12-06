f5ea2fcbc56536a8e631b62eb49cbb86e71823f3
nguyen phu hai <nguyenphuhai89@gmail.com>
2022-12-06 23:20:41 +0700

    commit lab2.4

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const express = require('express')
 
 const app =express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware!');
-    next();// chuyển đến middleware tiếp theo
+app.use('/',(req, res, next)=>{
+    console.log('1...This always runs!');
+    next();
 })
 
-app.use((req,res,next)=>{
-    console.log('in another middleware!');
+app.use('/add-product',(req,res,next)=>{
+    console.log('2....in the middleware1!');
+    res.send('<h1>The "Add product" Page</h1>')
+})
+
+app.use('/',(req,res,next)=>{
+    console.log('3...in another middleware!');
     res.send('<h1>hello from express</h1>')
 })
 
