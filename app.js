53533f7ad459a997465f16d36c37fc31d1bbd486
nguyen phu hai <nguyenphuhai89@gmail.com>
2022-12-06 16:09:15 +0700

    commit lab2.2

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const express = require('express')
 
 const app =express();
 
+app.use((req,res,next)=>{
+    console.log('in the middleware!');
+    next();// chuyển đến middleware tiếp theo
+})
+
+app.use((req,res,next)=>{
+    console.log('in another middleware!');
+})
+
 const server =http.createServer(app);
 
 server.listen(3000);
