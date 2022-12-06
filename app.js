86f4046814c84722c5a41ae3f13b2e9022e51500
nguyen phu hai <nguyenphuhai89@gmail.com>
2022-12-06 16:15:45 +0700

    commit lab2.3

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use((req,res,next)=>{
 
 app.use((req,res,next)=>{
     console.log('in another middleware!');
+    res.send('<h1>hello from express</h1>')
 })
 
 const server =http.createServer(app);
