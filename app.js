483e7f663e6151e0c52650c60b8a82423dd5e1d7
nguyen phu hai <nguyenphuhai89@gmail.com>
2022-12-07 06:53:58 +0700

    commit lab2.13

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app =express();
+const rootDir = require('./util/path');
 
 const bodyParser =require('body-parser');
 
@@ -14,7 +15,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'))
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express  = require('express');
 const path = require('path');
 const router = express.Router();
+const rootDir = require('../util/path')
 // admin/add-product => GET
 router.get('/add-product',(req,res,next)=>{
     console.log('1...in the middleware1!');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 // admin/product =>POST
 router.post('/product',(req,res,next)=>{
