e83a9a64d50a61c88c96e31051f1acda3688314b
nguyen phu hai <nguyenphuhai89@gmail.com>
2022-12-16 07:24:33 +0700

    commit lab4.6

--- app.js
+++ app.js
@@ -2,24 +2,18 @@ const express = require('express')
 const path = require('path');
 const app =express();
 const errorController =require('./controllers/error')
+const rootDir = require('./util/path');
+const bodyParser =require('body-parser');
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 app.set('view engine', 'ejs');
-app.set('views','views');
-
-const rootDir = require('./util/path');
-
-const bodyParser =require('body-parser');
-
-const adminRoutes = require('./routes/admin')
-
-const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404)
 
 app.listen(3000);
